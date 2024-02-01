d7ae63b4d2a77e24efdf4cc0687de5ed2a2a213f
mbeyerstjerne <mbeyerstjerne@gmail.com>
2024-02-01 20:19:13 +0100

    updated

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -3,17 +3,13 @@ import re
 sum=0
 groups=[]
 
-with open("day_01_input.txt", "r") as f:
+with open("day_01\day_01_input.txt", "r") as f:
     for line in f: 
-        print(f.readline(1))
-
-'''
-        first = re.search(r'\d+', line)
-        last = re.search(r'\d+', line[::-1])
+        first = re.search(r'\d', line)
+        last = re.search(r'\d', line[::-1])
         calibration = 10*first + 1*last
         groups.append(calibration)
 
 calibration_total = groups.sum()
 
 calibration_total
-'''
